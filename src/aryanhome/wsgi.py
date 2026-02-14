0ff8bbb7225de1f3a8996d4f596669980d18f5f2
aryan2ab <aryanbansal2.ab@gmail.com>
2026-02-14 17:05:00 +0530

    Hello World msg

---
+++ src/aryanhome/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for aryanhome project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aryanhome.settings')
+
+application = get_wsgi_application()
