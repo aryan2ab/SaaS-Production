13c1c0cf56f091a560323eaef58924035d87b806
aryan2ab <aryanbansal2.ab@gmail.com>
2026-02-14 17:15:22 +0530

    Updated views to change order

--- src/aryanhome/views.py
+++ src/aryanhome/views.py
@@ -8,6 +8,7 @@ def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visist_count": page_qs.count(),
@@ -15,7 +16,7 @@ def home_page_view(request, *args, **kwargs):
         "total_visit_count": qs.count(),
     }
     
-    html_template = "home.html"
+    
     PageVisit.objects.create(path=request.path)
     #html_file_path = this_dir / "home.html"
     #html_ = html_file_path.read_text()
