7f668867f6caffc752605612f2c6b40e305dd2c7
aryan2ab <aryanbansal2.ab@gmail.com>
2026-02-18 15:29:27 +0530

    .

--- src/aryanhome/settings.py
+++ src/aryanhome/settings.py
@@ -15,7 +15,22 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST=config("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT=config("EMAIL_PORT", default='587', cast=str)
+EMAIL_USE_TLS=config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_USE_SSL=config("EMAIL_USE_SSL", default=False, cast=bool)
+EMAIL_HOST_USER=config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=config("EMAIL_HOST_PASSWORD")
 
+ADMIN_USER_NAME = config("ADMIN_USER_NAME", default="Admin user")
+ADMIN_USER_EMAIL = config("ADMIN_USER_EMAIL", default=None)
+
+MANAGERS = []
+ADMINS=[]
+if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
+    ADMINS+=[f'{ADMIN_USER_NAME}', f'{ADMIN_USER_EMAIL}']
+    MANAGERS=ADMINS
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
