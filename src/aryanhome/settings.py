c5e1c08548b7a18925f7006b4ac678271cd3f79d
aryan2ab <aryanbansal2.ab@gmail.com>
2026-02-20 17:32:18 +0530

    .

--- src/aryanhome/settings.py
+++ src/aryanhome/settings.py
@@ -20,8 +20,9 @@ EMAIL_HOST=config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT=config("EMAIL_PORT", default='587', cast=str)
 EMAIL_USE_TLS=config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_USE_SSL=config("EMAIL_USE_SSL", default=False, cast=bool)
-EMAIL_HOST_USER=config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD=config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+
 
 ADMIN_USER_NAME = config("ADMIN_USER_NAME", default="Admin user")
 ADMIN_USER_EMAIL = config("ADMIN_USER_EMAIL", default=None)
